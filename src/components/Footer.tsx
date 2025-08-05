7856916f496ed0ec0b3b731f784cc87a1719e72e
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-05 17:35:34 +0100

    refactored footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,30 @@
+import { contacts } from "../utils";
+import Time from "./Time";
+
+export default function Footer() {
+	return (
+		<footer className="px-4 py-8 lg:max-w-5xl lg:mx-auto">
+			<div className="flex items-center gap-6">
+				{contacts.map(({ name, Icon, link }) => (
+					<a key={name} href={link}>
+						<Icon
+							width={20}
+							height={20}
+							className="inline-block mr-1 fill-current"
+						/>
+						{name}
+					</a>
+				))}
+			</div>
+			<div className="flex items-center justify-between mt-[5rem]">
+				<small>Built with a step out of comfort</small>
+				<span className="text-sm">
+					Lagos <Time />
+				</span>
+			</div>
+			<div className="text-center text-sm">
+				<p className="opacity-70 mt-4">©Enoch Fagbenja. All Rights reserved</p>
+			</div>
+		</footer>
+	);
+}
--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,6 +1,5 @@
-import { contacts } from "../../utils";
+import Footer from "../Footer";
 import Layout from "../Layout";
-import Time from "../Time";
 
 export default function About() {
 	return (
@@ -10,31 +9,7 @@ export default function About() {
 					<h1 className="text-7xl font-bold lg:text-7xl">About Me</h1>
 				</section>
 
-				<footer className="px-4 py-8 lg:max-w-5xl lg:mx-auto">
-					<div className="flex items-center gap-6">
-						{contacts.map(({ name, Icon, link }) => (
-							<a key={name} href={link}>
-								<Icon
-									width={20}
-									height={20}
-									className="inline-block mr-1 fill-current"
-								/>
-								{name}
-							</a>
-						))}
-					</div>
-					<div className="flex items-center justify-between mt-[5rem]">
-						<small>Built with a step out of comfort</small>
-						<span className="text-sm">
-							Lagos <Time />
-						</span>
-					</div>
-					<div className="text-center text-sm">
-						<p className="opacity-70 mt-4">
-							©Enoch Fagbenja. All Rights reserved
-						</p>
-					</div>
-				</footer>
+				<Footer />
 			</div>
 		</Layout>
 	);
