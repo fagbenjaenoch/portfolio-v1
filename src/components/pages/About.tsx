182adab40ded18548918a68a81fe44a03e26dc28
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-05 17:50:48 +0100

    moved back-to-home button to bottom

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router";
 import Footer from "../Footer";
 import Layout from "../Layout";
 
 export default function About() {
 	return (
 		<Layout>
-			<section className="px-4 lg:max-w-5xl lg:mx-auto h-[100dvh] pt-[10rem]">
+			<section className="px-4 lg:max-w-5xl lg:mx-auto pt-[10rem]">
 				<h1 className="text-7xl font-bold lg:text-7xl">About Me</h1>
 			</section>
-
+			<section className="px-4 lg:max-w-5xl lg:mx-auto">
+				<Link to={"/"} className="underline underline-offset-4 block">
+					⬅️ Go back to home
+				</Link>
+			</section>
 			<Footer />
 		</Layout>
 	);
