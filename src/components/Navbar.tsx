2e7eed232615764e95a11b23148d4a3c22114b94
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-05 17:43:23 +0100

    changed links from browser default to react router's

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 type navlink = {
 	title: string;
 	href: string;
@@ -22,12 +24,12 @@ export default function Navbar() {
 	return (
 		<section className="backdrop-blur-md fixed top-0 w-[100%] z-10">
 			<div className="px-4 py-6 lg:max-w-5xl lg:mx-auto flex items-center justify-between">
-				<span className="">Enoch Fagbenja</span>
+				<Link to={"/"}>Enoch Fagbenja</Link>
 				<div className="flex gap-4">
 					{navlinks.map(({ title, href }) => (
-						<a key={title} href={href}>
+						<Link key={title} to={href}>
 							{title}
-						</a>
+						</Link>
 					))}
 				</div>
 			</div>
