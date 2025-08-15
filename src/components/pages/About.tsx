762ca4732266f669d6615978df79fa5648450ee3
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-15 05:24:08 +0100

    made type fixes

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -7,51 +7,41 @@ export default function About() {
 	return (
 		<Layout>
 			<section className="px-4 lg:max-w-5xl lg:mx-auto pt-[10rem]">
-				<h1 className="text-7xl font-bold lg:text-7xl">About Me</h1>
+				<span className="inline-block text-secondary-muted ml-4">A bit</span>
+				<h1 className="text-7xl font-bold lg:text-7xl mt-[-5px]">About me</h1>
 			</section>
 			<section className="px-4 lg:max-w-5xl lg:mx-auto mt-8 relative">
-				<img
-					src={profilePhoto}
-					alt="Enoch"
-					className="w-[300px] float-right p-4"
-				/>
-				<p>Hi, I'm Enoch </p>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum debitis
-					iure facilis distinctio error, eligendi sequi maiores impedit ducimus
-					aperiam voluptate eos eveniet itaque suscipit? Quia blanditiis
-					repellendus quasi aut dolorum, placeat consequatur corporis. Harum eos
-					voluptatum iure, quis maxime modi ipsa impedit iusto doloribus
-					quibusdam alias autem enim eaque sit nostrum nihil nesciunt
-					repellendus omnis architecto laborum deserunt placeat eligendi
-					blanditiis culpa. Quasi saepe totam labore hic natus! Eaque sint
-					tempora rem aliquam veniam autem pariatur voluptatum ex consequatur
-					expedita quibusdam perferendis officia quo, neque, deserunt eius
-					nostrum quas veritatis voluptates. Nostrum corrupti, magnam iste
-					temporibus ipsam atque eaque eius libero quis voluptates illum
-					molestiae impedit. Pariatur vero accusamus unde doloribus quis
-					dignissimos optio accusantium odit consectetur, ratione magni amet
-					repellendus repellat mollitia consequuntur maiores. Aspernatur ipsum
-					nostrum eveniet a labore est et. Mollitia maxime rerum id nisi eius,
-					saepe, minus nobis quidem veniam nemo error omnis laborum ducimus
-					provident illo doloremque hic accusamus dolorem deleniti eveniet neque
-					inventore, vel facere. Vero odit iure reiciendis quas placeat fugiat
-					ut! Autem animi accusantium officia iusto saepe quod, vitae amet in
-					quasi facilis explicabo hic? Impedit molestiae facere quas laudantium,
-					est officiis placeat alias enim omnis voluptatum quaerat laboriosam
-					adipisci? Ea adipisci iure consectetur architecto dolorem, corrupti
-					pariatur ratione ducimus quae quisquam explicabo atque doloremque et!
-					Dolor perspiciatis, repellendus aut corrupti neque obcaecati quas quae
-					at maiores dolores cum, consequatur nemo! Illo et aliquid distinctio
-					aliquam molestias, alias officia assumenda aut nobis vitae odit nihil
-					tenetur corrupti eligendi rerum eveniet porro id explicabo vero
-					laudantium. Veritatis magni consequuntur perferendis adipisci autem
-					iusto qui quaerat. Commodi exercitationem unde sed labore eius. Quo
-					harum laboriosam temporibus mollitia voluptatibus illo a explicabo
-					omnis quam tempora consequatur odit incidunt aut perferendis dolores
-					nobis, quae magnam quod natus quis, nulla blanditiis iure modi? Quas,
-					eius nulla.
-				</p>
+				<div className="space-y-2 min-h-5/6">
+					<img
+						src={profilePhoto}
+						alt="Enoch"
+						className="w-[300px] float-right p-4"
+					/>
+					<p>Hi, I'm Enoch and I'm fascinated by computers.</p>
+					<p>
+						I got the opportunity to use one when my dad got a Toshiba{" "}
+						<i>circa 2010</i> and ever since then, I have constantly been awed
+						by the engineering behind it.
+					</p>
+					<p>
+						After years of dabbling as a very young enthusiast, I outgrew
+						regular use and playing games (I still do!) and desired to do{" "}
+						<i>more</i>.
+					</p>
+					<p>
+						Long story cut short, during my holiday breaks in secondary school,
+						I picked up Graphic Design, Video editing, PHP and MYSQL. I built a
+						simple dashboard application that retrieves (or was supposed to XD)
+						user mailing information and organize them. I also implemented basic
+						authentication and authorization features for the app. Looking back
+						in hindsight, I'm grateful I built it, it gave me the motivation to
+						learn web applications further.
+					</p>
+					<p>
+						Recently, I've worked with a few startups that built products for
+						hundreds of users and it exposed me to professional environments.
+					</p>
+				</div>
 				<Link to={"/"} className="underline underline-offset-4 block">
 					⬅️ Go back to home
 				</Link>
