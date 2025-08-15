4156860718ed4d44641d042206d3315db0f722c6
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-15 21:18:34 +0100

    added design artifact to navlinks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,9 +25,9 @@ export default function Navbar() {
 		<section className="backdrop-blur-md fixed top-0 w-[100%] z-10">
 			<div className="px-4 py-6 lg:max-w-5xl lg:mx-auto flex items-center justify-between text-sm lg:text-base">
 				<Link to={"/"}>Enoch Fagbenja</Link>
-				<div className="flex gap-4">
+				<div className="flex gap-4 lg:gap-8">
 					{navlinks.map(({ title, href }) => (
-						<NavLink key={title} to={href}>
+						<NavLink key={title} to={href} className="navlink">
 							{title}
 						</NavLink>
 					))}
